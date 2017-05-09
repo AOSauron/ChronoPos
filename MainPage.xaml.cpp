102c48bafdd21019e165d9c86eadea70cbbcba04
Gauthier ZAMBAUX <Gauthier ZAMBAUX>
2017-05-09 15:46:15 +0200

    Voilà ça fonctionne ! :D

--- MainPage.xaml.cpp
+++ MainPage.xaml.cpp
@@ -37,62 +37,27 @@ using namespace Windows::System::Display;
 
 // Pour en savoir plus sur le modèle d'élément Page vierge, consultez la page http://go.microsoft.com/fwlink/?LinkId=234238
 
-//void ChronoThreadProc();
-//void actualizeDisplay(TextBlock^ chronoValue);
 
 int hours, minutes, seconds;
 int running;
 int canBeActualized;
 std::mutex chrono_mutex;
 std::mutex gps_mutex;
-//Geolocator ^geolocator;
 String ^positionString;
-
-/*
-template <typename... ParamTypes>
-void setTimeOut(int milliseconds, std::function<void(ParamTypes...)> func, ParamTypes... parames)
-{
-	std::async(std::launch::async, [=]()
-	{
-		while (1) {
-			Sleep(milliseconds);
-			func(parames...);
-		}
-	});
-};*/
 
 
 
 
 MainPage::MainPage()
 {
-	//mainThreadHandling = new MainClass();
 	running = 0;
 	hours = 0; minutes = 0; seconds = 0;
 	canBeActualized = 0;
-	//geolocator = ref new Geolocator();
-	positionString = "Nan";
+	positionString = "";
 
-	/*std::mutex locker;
-	std::function<void()> actualizeDisplay = [&]()
-	{
-		chrono_mutex.lock();
-		chronoValue->Text = hours + ":" + minutes + ":" + seconds;
-		std::unique_lock<std::mutex> lk(locker);
-		//std::cout << "func 3 is trigged:" << "   int: " << param1 << ";  string: " << param2 << std::endl;
-		//lk.unlock();
-		chrono_mutex.unlock();
-	};
-
-	for (int index = 0; index < 100; index++) {
-		std::unique_lock<std::mutex> lk3(locker);
-		lk3.unlock();
-		setTimeOut<>(1000, actualizeDisplay);
-	}
-	*/
 	InitializeComponent();
 
-
+	startGPSThread();
 }
 
 /// <summary>
@@ -116,14 +81,12 @@ void MainPage::OnNavigatedTo(NavigationEventArgs^ e)
 
 void MainPage::startChronoThread() {
 	running = 1;
-	//std::thread chronoThread(ChronoThreadProc);
 	CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)ChronoThreadProc, NULL, 0, NULL);
 }
 
 void MainPage::ChronoThreadProc()
 {
 	Sleep(1000);
-	// Pendant que le thread principal est en attente :
 	while (running) {
 		
 		chrono_mutex.lock();
@@ -141,33 +104,63 @@ void MainPage::ChronoThreadProc()
 		Sleep(1000);
 	}
 }
-/*
-void CALLBACK ProjetSEP::MainPage::actualize(HWND hwnd, UINT uMsg, UINT timerId, DWORD dwTime) {
-	chronoValue->Text = hours + ":" + minutes + ":" + seconds;
-}*/
 
-
-
-/*void MainPage::startGPSThread() {
-	running = 1;
-	//std::thread chronoThread(ChronoThreadProc);
+void MainPage::startGPSThread() {
 	CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)GPSThreadProc, NULL, 0, NULL);
-}*/
+}
 
-/*void MainPage::GPSThreadProc() {
-	IAsyncOperation<Geoposition ^> ^asyncPos = geolocator->GetGeopositionAsync();
+void MainPage::GPSThreadProc()
+{
+	while (1) {
+		int accuracyInMeters = 5;
+		IAsyncOperation<Geoposition^>^ m_getOperation;
 
-	Geoposition ^position = await geolocator.GetGeopositionAsync();
-	try {
-		position = asyncPos->GetResults();
+		Geolocator^ geolocator = ref new Geolocator();
 
+		geolocator->DesiredAccuracyInMeters = (Platform::IBox<UINT>^)(PropertyValue::CreateUInt32(accuracyInMeters));
 
-		positionString = position->Coordinate->Latitude.ToString() + " - " + position->Coordinate->Longitude.ToString();
+		m_getOperation = geolocator->GetGeopositionAsync();
+
+		// Start location acquisition.
+		// Setting the completion callback implicitly starts acquisition.
+		m_getOperation->Completed = ref new AsyncOperationCompletedHandler<Geoposition^>(
+			[=](IAsyncOperation<Geoposition^>^ asyncOperation, AsyncStatus status) mutable
+		{
+			if (status != AsyncStatus::Error)
+			{
+				Geoposition^ geoposition = asyncOperation->GetResults();
+
+				// use the location information
+				double latitude = geoposition->Coordinate->Latitude;
+				double longitude = geoposition->Coordinate->Longitude;
+				double accuracy = geoposition->Coordinate->Accuracy;
+
+				//positionString = "Lat. " + latitude + ", Long. " + longitude + ", Précision = " + accuracy;
+				gps_mutex.lock();
+				positionString = "(" + latitude + " ; " + longitude + ")";//"Lat. " + latitude + ", Long. " + longitude;
+				gps_mutex.unlock();
+			}
+			else
+			{
+				if (asyncOperation->ErrorCode.Value == E_ABORT)
+				{
+					//positionString = "GPS DESACTIVE";
+					// The user has disable location in the phone settings
+					// Printer : Activer les GPS pour acquisition
+					perror("Le GPS est desactivé");
+				}
+				else
+				{
+					// L EMULATION SORT ICI !!!!!!
+					positionString = "ERREUR GPS";
+					// There was another error
+					// Printer : une erreur est survenue avec le GPS
+					perror("Une erreur est survenue avec le GPS");
+				}
+			}
+		});
 	}
-	catch (Platform::Exception^ ex) {
-
-	}
-}*/
+}
 
 
 
@@ -196,30 +189,6 @@ void MainPage::actualize(Platform::Object^ sender, Platform::Object^ e) {
 
 	chronoValue->Text = hourString + ":" + minuteString + ":" + secondString;
 }
-/*
-void MainPage::startActualizeDisplay() {
-	//std::thread chronoThread(ChronoThreadProc);
-	CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)ActualizeDisplay, (LPVOID)this, 0, NULL);
-}*/
-/*
-void MainPage::ActualizeDisplay(MainPage^ page) {
-	page = (MainPage^)page;
-	while (running) {
-		page->actualize();
-	}
-}*/
-/*
-void MainPage::waitForActualize() {
-	while (1) {
-		Sleep(500);
-		chrono_mutex.lock();
-		if (canBeActualized) {
-			chronoValue->Text = hours + ":" + minutes + ":" + seconds;
-			canBeActualized = 0;
-		}
-		chrono_mutex.unlock();
-	}
-}*/
 
 
 void MainPage::startButton_Click(Platform::Object^ sender, Windows::UI::Xaml::RoutedEventArgs^ e)
@@ -230,8 +199,6 @@ void MainPage::startButton_Click(Platform::Object^ sender, Windows::UI::Xaml::Ro
 		running = 1;
 		startButton->Content = "Pause";
 		this->startChronoThread();
-
-
 		
 		timer->Tick += ref new Windows::Foundation::EventHandler<Object^>(this, &MainPage::actualize);
 		TimeSpan t;
@@ -245,10 +212,6 @@ void MainPage::startButton_Click(Platform::Object^ sender, Windows::UI::Xaml::Ro
 		startButton->Content = "Démarrer";
 	}
 
-	/*
-	chronoThread.join();
-	chronoThread.join();
-	*/
 }
 
 
@@ -266,79 +229,36 @@ void MainPage::initializeButton_Click(Platform::Object^ sender, Windows::UI::Xam
 
  
 void MainPage::getPositionButton_Click(Platform::Object^ sender, Windows::UI::Xaml::RoutedEventArgs^ e) {
+	if (positionString != "") {
+		String ^hourString;
+		String ^minuteString;
+		String ^secondString;
 
-	String ^hourString;
-	String ^minuteString;
-	String ^secondString;
 
-	int accuracyInMeters = 5;
-
-	Geolocator^ geolocator = ref new Geolocator();
-
-	geolocator->DesiredAccuracyInMeters = (Platform::IBox<UINT>^)(PropertyValue::CreateUInt32(accuracyInMeters));
-
-	m_getOperation = geolocator->GetGeopositionAsync();
-
-	// Start location acquisition.
-	// Setting the completion callback implicitly starts acquisition.
-	m_getOperation->Completed = ref new AsyncOperationCompletedHandler<Geoposition^>(
-		[=](IAsyncOperation<Geoposition^>^ asyncOperation, AsyncStatus status) mutable
-	{
-		if (status != AsyncStatus::Error)
-		{
-			Geoposition^ geoposition = asyncOperation->GetResults();
-
-			// use the location information
-			double latitude = geoposition->Coordinate->Latitude;
-			double longitude = geoposition->Coordinate->Longitude;
-			double accuracy = geoposition->Coordinate->Accuracy;
-
-			//positionString = "Lat. " + latitude + ", Long. " + longitude + ", Précision = " + accuracy;
-			positionString = "Lat. " + latitude + ", Long. " + longitude;
+		if (hours < 10) {
+			hourString = "0" + hours.ToString();
 		}
-		else
-		{
-			if (asyncOperation->ErrorCode.Value == E_ABORT)
-			{
-				positionString = "GPS DESACTIVE";
-				// The user has disable location in the phone settings
-				// Printer : Activer les GPS pour acquisition
-				perror("Le GPS est desactivé");
-			}
-			else
-			{
-				// L EMULATION SORT ICI !!!!!!
-				positionString = "ERREUR GPS";
-				// There was another error
-				// Printer : une erreur est survenue avec le GPS
-				perror("Une erreur est survenue avec le GPS");
-			}
+		else {
+			hourString = hours.ToString();
 		}
-	});
+		if (minutes < 10) {
+			minuteString = "0" + minutes.ToString();
+		}
+		else {
+			minuteString = minutes.ToString();
+		}
+		if (seconds < 10) {
+			secondString = "0" + seconds.ToString();
+		}
+		else {
+			secondString = seconds.ToString();
+		}
 
-	if (hours < 10) {
-		hourString = "0" + hours.ToString();
+		//positionString = "\n Lat. 2566\n Long. 3648";
+
+		String^ temp = savedValues->Text;
+		gps_mutex.lock();
+		savedValues->Text = hourString + ":" + minuteString + ":" + secondString + "\n" + positionString + "\n" + temp;
+		gps_mutex.unlock();
 	}
-	else {
-		hourString = hours.ToString();
-	}
-	if (minutes < 10) {
-		minuteString = "0" + minutes.ToString();
-	}
-	else {
-		minuteString = minutes.ToString();
-	}
-	if (seconds < 10) {
-		secondString = "0" + seconds.ToString();
-	}
-	else {
-		secondString = seconds.ToString();
-	}
-	
-	//positionString = "\n Lat. 2566\n Long. 3648";
-	
-	String^ temp = savedValues->Text;
-	gps_mutex.lock();
-	savedValues->Text = hourString + ":" + minuteString + ":" + secondString + " " + positionString + "\n" + temp;
-	gps_mutex.unlock();
 }
--- MainPage.xaml.h
+++ MainPage.xaml.h
@@ -7,7 +7,6 @@
 
 #include "MainPage.g.h"
 #include <windows.h>
-//#include "MainClass.h"
 
 using namespace Windows::UI::Xaml::Controls;
 
@@ -26,22 +25,13 @@ namespace ProjetSEP
 		virtual void OnNavigatedTo(Windows::UI::Xaml::Navigation::NavigationEventArgs^ e) override;
 
 	private:
-
-		//static DWORD WINAPI actualizeDisplay(MainPage^ page);
-		//void ChronoThreadProc();
 		void startButton_Click(Platform::Object^ sender, Windows::UI::Xaml::RoutedEventArgs^ e);
 		void initializeButton_Click(Platform::Object^ sender, Windows::UI::Xaml::RoutedEventArgs^ e);
 		void getPositionButton_Click(Platform::Object^ sender, Windows::UI::Xaml::RoutedEventArgs^ e);
-		//MainClass* mainThreadHandling;
 		void startChronoThread();
 		static void ChronoThreadProc();
-		/*void startGPSThread();
-		static void GPSThreadProc();*/
-		//void startActualizeDisplay();
-		//static void ActualizeDisplay(MainPage^ page);
+		void startGPSThread();
+		static void GPSThreadProc();
 		void actualize(Platform::Object^ sender, Platform::Object^ e);
-		//void waitForActualize();
-		//void CALLBACK actualize(HWND hwnd, UINT uMsg, UINT timerId, DWORD dwTime);
-		Windows::Foundation::IAsyncOperation<Windows::Devices::Geolocation::Geoposition^>^ m_getOperation;
 	};
 }
